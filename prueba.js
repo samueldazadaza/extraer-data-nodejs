080a0d04b8962d746a54e6f3a067cf47ad743588
Samuel Daza <samueldazadaza@gmail.com>
2021-07-08 22:07:40 -0500

    actualizar informacion cada cirto tiempo

--- prueba.js
+++ prueba.js
@@ -1,33 +1,15 @@
 const os = require('os');
 
-const data = os.networkInterfaces()
 
-//console.log(os.platform());
-//console.log(os.release());
-
-
-var wifi = require('node-wifi');
- 
-// Initialize wifi module
-// Absolutely necessary even to set interface to null
-wifi.init({
-  iface: null // network interface, choose a random wifi interface if set to null
-});
- 
 
 function myFunc(arg) {
+  
+  const data = os.networkInterfaces()
+  console.log(data)
 
-// List the current wifi connections
-wifi.getCurrentConnections((error, currentConnections) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(currentConnections([]));
-    }
-  });
 
 }
   
-  setInterval(myFunc, 1500);
+  setInterval(myFunc, 1500); // actualizar informacion cada cirto tiempo
 
 
