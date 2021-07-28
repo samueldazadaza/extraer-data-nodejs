228c48ac26dbb331c54e2991a1fb0e2eb578a8d8
sam <samueldazadaza@gmail.com>
2021-07-27 22:37:04 -0500

    cambiando busqueda wifi cada 2 seg :) (y)

--- wifi.js
+++ wifi.js
@@ -1,23 +1,23 @@
-var wifi = require('node-wifi');
-var nombre_wifi = "ZEUS"
- 
-// Initialize wifi module
-// Absolutely necessary even to set interface to null
-wifi.init({
-  iface: null // network interface, choose a random wifi interface if set to null
-});
- 
-// Scan networks
-
-    wifi.scan((error, networks) => {
-        if (error) {
-            console.log(error);
-        } else {
-            
-            
-            return        console.log(networks.find(networks => networks.ssid === nombre_wifi));
-            
-        }
+function extraer () {
+    var wifi = require('node-wifi');
+    var nombre_wifi = "TITO"
+     
+    // Initialize wifi module
+    // Absolutely necessary even to set interface to null
+    wifi.init({
+      iface: null // network interface, choose a random wifi interface if set to null
     });
+     
+    // Scan networks
+    wifi.scan((error, networks) => {
+      if (error) {
+        console.log(error);
+      } else {
     
-// setInterval(extraer, 2000); // actualizar informacion cada 2 seg
+        console.log(networks.find(networks => networks.ssid === nombre_wifi));
+      }
+    });
+};
+  extraer(); //correr funcion ver wifis
+  setInterval(extraer, 2000); // actualizar informacion cada 2 seg
+  
